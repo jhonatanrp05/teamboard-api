eb4c695310a39b43dcda349e23b5b31550656dea
jhonatanrp05 <jpachecos@uninorte.edu.co>
2026-05-15 23:55:06 -0500

    feat: add health check endpoint

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
